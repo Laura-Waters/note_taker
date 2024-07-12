b9cd66b7fb4f0d1ca01b53f79576e2380f033c57
Laura <laurawatersalston@gmail.com>
2024-07-11 22:08:55 -0400

    removes extra routes req

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001; 
-const routes = require('./routes');
 const api = require('./routes');
 
 // Middleware for parsing JSON data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-app.use(routes);
 app.use(express.static('public'));
 
 // GET Route for homepage
